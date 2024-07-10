5226e367bdc523ff0a949d3e315c4494935a2f74
MarioBrum <42178811+MarioBrum@users.noreply.github.com>
2024-07-10 10:55:00 -0300

    Main code upload

---
+++ minesweeper.java
@@ -0,0 +1,15 @@
+import java.util.Random();
+public class Minesweeper{
+	private char [][] board;
+	private final int rows = 16;
+	private final int columns = 16;
+	private final int bombs = 40;
+	public static void main(String [] args){
+		char[][] board = new char[rows][columns];
+	}
+	
+	public static createAGame(){
+	
+	}
+	
+}
