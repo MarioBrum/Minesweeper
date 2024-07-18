1194a2ce6ac68371cf93520225bd6642464cd3ef
MarioBrum <42178811+MarioBrum@users.noreply.github.com>
2024-07-18 10:58:00 -0300

    Update minesweeper.java
    Logical of the main board

--- minesweeper.java
+++ minesweeper.java
@@ -1,15 +1,32 @@
 import java.util.Random();
 public class Minesweeper{
-	private char [][] board;
-	private final int rows = 16;
-	private final int columns = 16;
-	private final int bombs = 40;
+	private int [][] board;
+	private final int rows = 9;
+	private final int columns = 9;
+	private final int bombs = 10;
 	public static void main(String [] args){
-		char[][] board = new char[rows][columns];
+		//0 equals nothing
+		//-1 equals bomb
+		int[][] board = new int[rows][columns];
+		
 	}
 	
-	public static createAGame(){
-	
+	public static void createAGame(){
+		for(int i = 0; i < 
+	}
+
+	//return -1 if is a bomb
+	//return 0 if is not a bomb and dont have bombs adjacents
+	//return a number of bombs adjacents
+	/*			x=2 y=2
+	1,1| 1,2 | 1,3 		x-1,y-1| x-1,y | x-1,y+1 
+	2,1| 2,2 | 2,3		x,y-1  | x,y   | x,y+1
+	3,1| 3,2 | 3,3		x+1,y-1| x+1,y | x+1,y+1
+	*/
+	public static int returnAdjacents(int x,int y){
+		int cont = 0;
+		
+		
 	}
 	
 }
