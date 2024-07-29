776f9728fffc9ecfbb43e7af6a038c5379ef16b4
MarioBrum <42178811+MarioBrum@users.noreply.github.com>
2024-07-29 17:42:13 -0300

    Update minesweeper.java

--- minesweeper.java
+++ minesweeper.java
@@ -1,18 +1,39 @@
-import java.util.Random();
-public class Minesweeper{
-	private int [][] board;
-	private final int rows = 9;
-	private final int columns = 9;
-	private final int bombs = 10;
+import java.util.Random;
+public class minesweeper{
+	public static int [][]board;
+	public static final int rows = 9;
+	public static final int columns = 9;
+	public static final int bombs = 10;
+	public static Random r;
+	
 	public static void main(String [] args){
 		//0 equals nothing
 		//-1 equals bomb
-		int[][] board = new int[rows][columns];
+		this.board = new int[this.rows][this.columns];
+		r = new Random();
+		createGame(this.board);
+		printBoard(this.board);
 		
 	}
-	
-	public static void createAGame(){
-		for(int i = 0; i < 
+	public static void printBoard(int[][] board){
+		for(int i = 0; i < rows; i++){
+			for(int j = 0; j < columns; j++){
+				System.out.print(board[i][j]);	
+			}	
+			System.out.println();
+		}
+	}
+	public static void createGame(int[][] board){
+		int auxBombs = 0;
+		while(bombs > auxBombs){
+			for(int i = 0; i < rows; i++){
+				for(int j = 0; j < columns; j++){
+					if(r.nextInt(5) == 0){
+						board[i][j] = -1;
+					}	
+				}
+			}
+		}
 	}
 
 	//return -1 if is a bomb
@@ -23,10 +44,11 @@ public class Minesweeper{
 	2,1| 2,2 | 2,3		x,y-1  | x,y   | x,y+1
 	3,1| 3,2 | 3,3		x+1,y-1| x+1,y | x+1,y+1
 	*/
+	/*
 	public static int returnAdjacents(int x,int y){
 		int cont = 0;
 		
 		
 	}
-	
+	*/
 }
